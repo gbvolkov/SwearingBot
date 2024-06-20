7b79a4bcecc8489cf4c1d2184fe0d2a2528eda95
Georgy <gbvolkov@gmail.com>
2024-06-20 10:17:20 +0300

    2

--- config.py
+++ config.py
@@ -2,4 +2,6 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv(os.path.join(os.path.expanduser("~/Documents"), 'gv.env'))
-TELEGRAM_BOT_TOKEN = "7055842831:AAFaeeZt_2F1ccZ6LWq26IPHhaRAnTSG7aQ"
+OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
+ELEVENLABS_API_KEY = os.environ.get('ELEVENLABS_API_KEY')
+
