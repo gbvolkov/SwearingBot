e47e7f1a956cab95b25b50f067789ec1e09c5b57
GBV <gbvolkov@gmail.com>
2024-11-29 12:10:44 +0300

    1

--- config.py
+++ config.py
@@ -16,3 +16,4 @@ class Config:
     GIGA_CHAT_AUTH = os.environ.get('GIGA_CHAT_AUTH')
     NEWSAPI_API_KEY = os.environ.get('NEWSAPI_API_KEY')
     SWEAR_PROMPT = os.environ.get('SWEAR_PROMPT')
+    SILERO_LOCAL_PATH = os.environ.get('SILERO_LOCAL_PATH')
